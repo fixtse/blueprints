f634a5df8dfcf815a11bb60e975631863fa6482b
Fixt <fixtse@gmail.com>
2024-01-24 02:25:58 +0000

    feat: :zap: wsl script

---
+++ wsl/WSLPorts.ps1
@@ -0,0 +1,28 @@
+$ports = @(11434, 3000);
+
+$wslAddress = bash.exe -c "ip a | grep eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
+
+if ($wslAddress -match '^(\d{1,3}\.){3}\d{1,3}$') {
+  Write-Host "WSL IP address: $wslAddress" -ForegroundColor Green
+  Write-Host "Ports: $ports" -ForegroundColor Green
+}
+else {
+  Write-Host "Error: Could not find WSL IP address." -ForegroundColor Red
+  exit
+}
+
+$listenAddress = '0.0.0.0';
+
+foreach ($port in $ports) {
+  Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$listenAddress";
+  Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$listenAddress connectport=$port connectaddress=$wslAddress";
+}
+
+$fireWallDisplayName = "WSLPorts";
+$portsStr = $ports -join ",";
+
+Invoke-Expression "Remove-NetFireWallRule -DisplayName $fireWallDisplayName";
+Invoke-Expression "New-NetFireWallRule -DisplayName $fireWallDisplayName -Direction Outbound -LocalPort $portsStr -Action Allow -Protocol TCP";
+Invoke-Expression "New-NetFireWallRule -DisplayName $fireWallDisplayName -Direction Inbound -LocalPort $portsStr -Action Allow -Protocol TCP";
+
+wsl --exec dbus-launch true
